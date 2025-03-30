38a2e4a0eacba1cbea2ac6d06315ee770d0ad84c
amit <amit@gmail.com>
2025-03-30 18:39:09 +0530

    firstrt

--- app/lib/dbConnect.js
+++ app/lib/dbConnect.js
@@ -1,29 +1,17 @@
-// import mongoose from 'mongoose';
-
-// const dbConnect = async () => {
-//   if (mongoose.connection.readyState >= 1) return;
-//   try {
-//     const MONGO_URI = process.env.MONGO_URI;
-//     if (!MONGO_URI) {
-//       throw new Error("MongoDB URI is not defined");
-//     }
-//     await mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-//     console.log("MongoDB Connected");
-//   } catch (error) {
-//     console.error("MongoDB connection error:", error);
-//   }
-// };
-
-// export default dbConnect;
-
-
 import mongoose from 'mongoose';
 
 const dbConnect = async () => {
   if (mongoose.connection.readyState >= 1) return;
-  return mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true, // Deprecated, remove this line
-    useUnifiedTopology: true, // You can keep this
-  });
+  try {
+    const MONGO_URI = process.env.MONGO_URI;
+    if (!MONGO_URI) {
+      throw new Error("MongoDB URI is not defined");
+    }
+    await mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+    console.log("MongoDB Connected");
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+  }
 };
-export default dbConnect
+
+export default dbConnect;
